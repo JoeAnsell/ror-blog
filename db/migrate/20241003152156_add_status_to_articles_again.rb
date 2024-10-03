b7ebbecca6db7dc4daad7f1396adba3bb1efdbfd
Joseph <anselljoseph19@gmail.com>
2024-10-03 17:01:57 +0100

    re-added status to articles

---
+++ db/migrate/20241003152156_add_status_to_articles_again.rb
@@ -0,0 +1,5 @@
+class AddStatusToArticlesAgain < ActiveRecord::Migration[7.1]
+  def change
+    add_column :articles, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_02_160721) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_03_152156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,9 +47,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_160721) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.bigint "user_id"
     t.string "image"
+    t.string "status"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -59,7 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_160721) do
     t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
