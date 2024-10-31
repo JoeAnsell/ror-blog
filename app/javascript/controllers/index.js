45c2227278d09f82d2f8d0cb590757a27c0e2d08
Joseph <anselljoseph19@gmail.com>
2024-10-31 22:42:16 +0000

    assigned IDs to sticker buttons

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,3 @@ import { application } from "./application.js";
 
 import CanvasController from "./canvas_controller.js";
 application.register("canvas", CanvasController);
-
-import SimpleController from "./simple_controller.js";
-application.register("simple", SimpleController);
